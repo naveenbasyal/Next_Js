9a623d09988f74a5c59ae00c1947cfd1e8d37725
Naveen Basyal <naveenbasyal.001@gmail.com>
2023-02-14 11:31:30 +0530

    learned the routing without refresh using Link Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
-
+import Link from "next/link";
 const index = () => {
   return (
-    <h1>Hello World</h1>
-  )
-}
+    <>
+      <nav>
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/blogs">Blog</Link>
+          <Link href="/contact">Contact</Link>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default index
+export default index;
